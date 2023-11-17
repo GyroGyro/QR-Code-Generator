fd9bea74159093ee7a22ef84d78a7750c54df6e0
GyroGyro <42949338+GyroGyro@users.noreply.github.com>
2023-11-17 21:06:28 +0100

    Update QR Code Generator.py

--- QR Code Generator.py
+++ QR Code Generator.py
@@ -1,10 +1,10 @@
 import os
 import qrcode
-from PIL import Image
+from PIL import Image, ImageTk
 import tkinter as tk
 from tkinter import filedialog
 
-def generate_qr_code(data, file_name):
+def generate_qr_code(data, file_name, qr_folder):
     qr = qrcode.QRCode(
         version=5,
         error_correction=qrcode.constants.ERROR_CORRECT_L,
@@ -17,7 +17,6 @@ def generate_qr_code(data, file_name):
 
     img = qr.make_image(fill_color="black", back_color="white")
 
-    qr_folder = "QR Codes"
     if not os.path.exists(qr_folder):
         os.makedirs(qr_folder)
     
@@ -26,19 +25,37 @@ def generate_qr_code(data, file_name):
 
 def on_generate_button_click():
     link = link_entry.get()
-    file_name = file_name_entry.get()+".png"
+    file_name = file_name_entry.get() + ".png"
 
     if link and file_name:
-        generate_qr_code(link, file_name)
+        generate_qr_code(link, file_name, qr_folder)
         result_label.config(text=f"QR Code generated and saved as {file_name}")
     else:
         result_label.config(text="Enter link and file name!")
+
+def change_directory():
+    global qr_folder
+    new_directory = filedialog.askdirectory(title="Select QR Codes directory")
+    if new_directory:
+        qr_folder = new_directory
+
+
+def on_enter(event):
+    path_button.config(text=f"File Path: {qr_folder}")
+    path_button.place(relx=0.5, rely=1.0, anchor='s')
+    path_button.grid(row=7, column=0, columnspan=2)
+
+def on_leave(event):
+    path_button.place_forget()
+    path_button.grid_forget()
 
 # [✔] 🠗 Create the main window 🠗 
 root = tk.Tk()
 root.title("QR Code Generator")
 root.configure(bg='#122b49')
 root.iconbitmap(default="icon.ico")
+
+qr_folder = "QR_Codes"
 
 # [✔] 🠗 Create and place widgets 🠗 
 frame = tk.Frame(root, padx=20, pady=20, bg='#122b49', relief=tk.RIDGE, bd=2)  
@@ -60,10 +77,19 @@ generate_button = tk.Button(frame, text="Generate QR Code", command=on_generate_
 generate_button.grid(row=4, column=0, columnspan=2, pady=(10, 0))
 
 result_label = tk.Label(frame, text="", font=("Arial", 12), fg="#459bfa", bg='#122b49', pady=10)
-result_label.grid(row=5, column=0, columnspan=2)
+result_label.grid(row=7, column=0, columnspan=2)
+
+change_directory_button = tk.Button(frame, text="Change Directory", command=change_directory, font=("Arial", 12), bg="#459bfa", fg="white")
+change_directory_button.grid(row=5, column=0, columnspan=2, pady=(10, 0))
+change_directory_button.bind("<Enter>", on_enter)
+change_directory_button.bind("<Leave>", on_leave)
+
+path_button = tk.Label(frame, text="", font=("Arial", 10), bg='#122b49', fg='white')
+path_button.place_forget()
+path_button.grid_forget()
 
 author_name_label = tk.Label(frame, text="Author: ~Gყɾσ", font=("Arial", 10), bg='#122b49', fg='white')
-author_name_label.grid(row=7, column=0, columnspan=2, sticky="se")
+author_name_label.grid(row=8, column=0, columnspan=2, sticky="se")
 
 # [✔] 🠗 Start the event loop 🠗 
 root.mainloop()
